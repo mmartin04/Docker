93fb10b50e02daeb01652769a511d6240d5e0e93
John Doe~ <johndoe@example.com>
2023-12-18 20:07:01 +0100

    handler update

--- OrderService/handler/order.go
+++ OrderService/handler/order.go
@@ -4,35 +4,39 @@ import (
     "context"
     "encoding/json"
     "net/http"
+    "github.com/go-chi/chi/v5"
     "github.com/mmartin04/Docker/OrderService/db"
     "github.com/mmartin04/Docker/OrderService/model"
-    "github.com/go-chi/chi/v5"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type Order struct{}
 
-func (o *Order) Create(w http.ResponseWriter, r *http.Request) {
-    var order model.Order
-    err := json.NewDecoder(r.Body).Decode(&order)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+// Vorhandene Methoden: Create, GetByID
 
+// List - Listet alle Bestellungen auf
+func (o *Order) List(w http.ResponseWriter, r *http.Request) {
     collection := db.GetCollection("orders")
-    result, err := collection.InsertOne(context.TODO(), order)
+    cursor, err := collection.Find(context.TODO(), bson.M{})
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    defer cursor.Close(context.TODO())
 
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(result)
+    var orders []model.Order
+    if err = cursor.All(context.TODO(), &orders); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(orders)
 }
 
-func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
+// UpdateByID - Aktualisiert eine Bestellung anhand ihrer ID
+func (o *Order) UpdateByID(w http.ResponseWriter, r *http.Request) {
     id := chi.URLParam(r, "id")
     objID, err := primitive.ObjectIDFromHex(id)
     if err != nil {
@@ -41,10 +45,16 @@ func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
     }
 
     var order model.Order
-    collection := db.GetCollection("orders")
-    err = collection.FindOne(context.TODO(), bson.M{"_id": objID}).Decode(&order)
+    err = json.NewDecoder(r.Body).Decode(&order)
     if err != nil {
-        http.Error(w, err.Error(), http.StatusNotFound)
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    collection := db.GetCollection("orders")
+    _, err = collection.UpdateByID(context.TODO(), objID, bson.M{"$set": order})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
@@ -52,4 +62,21 @@ func (o *Order) GetByID(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(order)
 }
 
-// Weitere CRUD-Methoden hier implementieren...
+// DeleteByID - Löscht eine Bestellung anhand ihrer ID
+func (o *Order) DeleteByID(w http.ResponseWriter, r *http.Request) {
+    id := chi.URLParam(r, "id")
+    objID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    collection := db.GetCollection("orders")
+    _, err = collection.DeleteOne(context.TODO(), bson.M{"_id": objID})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+}
--- UserService/handler/user.go
+++ UserService/handler/user.go
@@ -4,35 +4,39 @@ import (
     "context"
     "encoding/json"
     "net/http"
+    "github.com/go-chi/chi/v5"
     "github.com/mmartin04/Docker/UserService/db"
     "github.com/mmartin04/Docker/UserService/model"
-    "github.com/go-chi/chi/v5"
     "go.mongodb.org/mongo-driver/bson"
     "go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type User struct{}
 
-func (u *User) Create(w http.ResponseWriter, r *http.Request) {
-    var user model.User
-    err := json.NewDecoder(r.Body).Decode(&user)
-    if err != nil {
-        http.Error(w, err.Error(), http.StatusBadRequest)
-        return
-    }
+// Vorhandene Methoden: Create, GetByID
 
+// List - Listet alle Benutzer auf
+func (u *User) List(w http.ResponseWriter, r *http.Request) {
     collection := db.GetCollection("users")
-    result, err := collection.InsertOne(context.TODO(), user)
+    cursor, err := collection.Find(context.TODO(), bson.M{})
     if err != nil {
         http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
+    defer cursor.Close(context.TODO())
 
-    w.WriteHeader(http.StatusCreated)
-    json.NewEncoder(w).Encode(result)
+    var users []model.User
+    if err = cursor.All(context.TODO(), &users); err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+    json.NewEncoder(w).Encode(users)
 }
 
-func (u *User) GetByID(w http.ResponseWriter, r *http.Request) {
+// UpdateByID - Aktualisiert einen Benutzer anhand seiner ID
+func (u *User) UpdateByID(w http.ResponseWriter, r *http.Request) {
     id := chi.URLParam(r, "id")
     objID, err := primitive.ObjectIDFromHex(id)
     if err != nil {
@@ -41,10 +45,16 @@ func (u *User) GetByID(w http.ResponseWriter, r *http.Request) {
     }
 
     var user model.User
-    collection := db.GetCollection("users")
-    err = collection.FindOne(context.TODO(), bson.M{"_id": objID}).Decode(&user)
+    err = json.NewDecoder(r.Body).Decode(&user)
     if err != nil {
-        http.Error(w, err.Error(), http.StatusNotFound)
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    collection := db.GetCollection("users")
+    _, err = collection.UpdateByID(context.TODO(), objID, bson.M{"$set": user})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
         return
     }
 
@@ -52,3 +62,21 @@ func (u *User) GetByID(w http.ResponseWriter, r *http.Request) {
     json.NewEncoder(w).Encode(user)
 }
 
+// DeleteByID - Löscht einen Benutzer anhand seiner ID
+func (u *User) DeleteByID(w http.ResponseWriter, r *http.Request) {
+    id := chi.URLParam(r, "id")
+    objID, err := primitive.ObjectIDFromHex(id)
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusBadRequest)
+        return
+    }
+
+    collection := db.GetCollection("users")
+    _, err = collection.DeleteOne(context.TODO(), bson.M{"_id": objID})
+    if err != nil {
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    w.WriteHeader(http.StatusOK)
+}
